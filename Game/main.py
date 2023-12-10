5f88b6d894bf37cda8e03d4ce099f78798b5fe99
trmdn <doludunyao@gmail.com>
2023-12-10 20:56:14 +0200

    Finally you can see the enemy in the display

--- Game/main.py
+++ Game/main.py
@@ -17,25 +17,6 @@ SCREEN_HEIGHT = 600
 
 pygame.display.set_caption("JUST A GAME")
 
-# Enemy
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Enemy, self).__init__()
-        self.surf = pygame.Surface((20, 10))
-        self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect(
-            center=(
-                random.randint(SCREEN_WIDTH + 20, SCREEN_HEIGHT + 100),
-                random.randint(0, SCREEN_HEIGHT),
-            )
-        )
-        self.speed = random.randint(5, 20)
-
-    def update(self):
-        self.rect.move_ip(-self.speed, 0)
-        if self.rect.right < 0:
-            self.kill()
-
 #Player
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -62,14 +43,41 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
+
+# Enemy
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
 # Initialize pygame
 pygame.init()
 
 # Create the screen object
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
 
 player = Player()
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 # Variable to keep the main loop running
 running = True
@@ -83,14 +91,24 @@ while running:
 
         elif event.type == QUIT:
             running = False
+        
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
 
     pressed_keys = pygame.key.get_pressed()
-
     player.update(pressed_keys)
 
+    enemies.update()
+
     screen.fill((0, 0, 0))
+
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
 
     screen.blit(player.surf, player.rect)
 
     pygame.display.flip()
+
 
